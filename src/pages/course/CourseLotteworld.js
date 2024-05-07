d5d782e32701de5b4486fe0d9f387336c28fc8fa
gyeomnii <a01020799303@gmail.com>
2024-05-08 00:10:57 +0900

    코스추천 작업중

--- src/pages/course/CourseLotteworld.js
+++ src/pages/course/CourseLotteworld.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import S from 'styled-components'
 
 const CourseLotteworld = () => {
   return (
     <div>
-      
+        <S.NavParkSelector>
+        <NavLink to={"/basiccost/lotteworld"}>
+          <p>롯데월드</p>
+        </NavLink>
+        <span>|</span>
+        <NavLink to={"/basiccost/everland"}>
+            <p>에버랜드</p>
+        </NavLink>
+        <span>|</span>
+        <NavLink to={"/basiccost/seoulland"}>
+          <p>서울랜드</p>
+        </NavLink>
+      </S.NavParkSelector>
     </div>
   );
 };
