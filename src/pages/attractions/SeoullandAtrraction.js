9d82ecb48c98d9eef5fc392055f1e25612f03351
gyeomnii <a01020799303@gmail.com>
2024-05-25 16:38:38 +0900

    어트랙션브랜치 수정완료

--- src/pages/attractions/SeoullandAtrraction.js
+++ src/pages/attractions/SeoullandAtrraction.js
@@ -1,67 +1,77 @@
 import React, { useEffect, useState } from 'react';
-import {  NavLink } from 'react-router-dom';
+import { NavLink }  from 'react-router-dom';
 import BasicSearch from '../../components/search/BasicSearch.jsx';
 import S from './style.js';
 
-const SeroullandAttraction = () => {
+const LotteworldAttraction = () => {
 
-    const [attractions, setAttractions] = useState([])
+
+    const [attractions, setAttractions] = useState([]);
+
     useEffect(() => {
         const getAttractions = async () => {
-            const response = await fetch(`http://localhost:8000/attraction/Seoulland`, {
+            const response = await fetch(`http://localhost:8000/attraction/seoulland`, {
                 method : 'POST',
                 credentials : 'include',
                 headers : {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 }
-            })
+            });
             const datas = await response.json()
+            console.log(datas)
             return datas;
-        }
-        getAttractions().then(setAttractions)
-    }, [])
+        };
+        getAttractions().then((datas)=>{
+            // console.log(datas);
+            setAttractions(datas);
+        });
+    }, []);
+  
+    
 
     // 화면에 뿌리기 위해 리스트를 구현한다.
     const attractionsList = attractions.map((att, i) => (
-        <S.ImageWrapper>
-        <NavLink key={i} to={`/attraction/seoulland/explain/${att.id}`}>
-            {/* <img src={`process.env.PUBLIC_URL/pages/attractions/lotte/blahblah${att.id}.jpg`} /> */}
-             <S.Image src={`${process.env.PUBLIC_URL}/AttractionsImage/Seoulland/at${att.id}.jpg`} alt="attraction" />
-            <div>
-                <p>{att.title}</p>
-                <span>{att.location}</span>
-                {/* <p>{att.discription}</p> */}
-            </div>  
-        </NavLink>
-        </S.ImageWrapper>
+     <S.ImageWrapper key={i}>
+        <NavLink to={`/attraction/seoulland/explain/${att.id}`}>    
+         <S.Image src={`${process.env.PUBLIC_URL}/AttractionsImage/Seoulland/at${att.id}.jpg`} alt="attraction" />
+           <div>
+             <span>{att.title}</span>
+             <p>{att.location}</p>
+           </div>  
+       </NavLink>
+     </S.ImageWrapper>
         
-    ))
-
+    ));
+   
     return (
-        <S.Background>
 
-            <S.NavParkSelector>
-                <NavLink to={"/attraction/lotteworld"}>롯데월드</NavLink>
-                <span>|</span>
-                <NavLink to={"/attraction/everland"}>에버랜드</NavLink>
-                <span>|</span>
-                <NavLink to={"/attraction/seoulland"}>서울랜드</NavLink>
-            </S.NavParkSelector>
+     <S.Background>
 
-        
+       <S.NavParkSelector>
+        <NavLink to={"/attraction/lotteworld"}>롯데월드</NavLink>
+         <span>|</span>
+         <NavLink to={"/attraction/everland"}>에버랜드</NavLink>
+         <span>|</span>
+         <NavLink to={"/attraction/seoulland"}>서울랜드</NavLink>
+       </S.NavParkSelector>
 
-            <S.search className='searchbar'>
-              <BasicSearch  shape={"default"} variant={"main"} size={"default"} />
-             </S.search>
+      <S.search className='searchbar'>
+       <BasicSearch  shape={"default"} variant={"main"} size={"default"} />
+      </S.search>
 
-            <div>
-                <S.AttractionsContainer>
-                {attractions && attractionsList}
-                </S.AttractionsContainer>
-            </div>
-        </S.Background>
+     <div>
+       <S.AttractionsContainer>
+         {attractions && attractionsList}
+       </S.AttractionsContainer>
+     </div>
+
+   </S.Background>
     );
 };
 
-export default SeroullandAttraction;
+
+
+
+export default LotteworldAttraction;
+
